d2af44504e6252dee91a0eb782d542058286f278
Jana <113575455+JanaAbhi@users.noreply.github.com>
2024-05-17 22:00:34 +0530

    adding reduce

--- index.js
+++ index.js
@@ -99,3 +99,43 @@ class FilterConst {
 let arr2 = new FilterConst(arr);
 let resultConFill = arr2.classOfFilter((num) => num % 2 == 0);
 console.log("Filter in Constructor:", resultConFill);
+
+/*==================== Reduce ====================*/
+
+/// Reduce in Array
+
+Array.prototype.myRed = function (callback) {
+ let acc=0;
+ for(let i=0;i<this.length;i++){
+   callback(acc=acc + this[i])
+ }
+ return acc
+
+}
+let val=[2,3,4,5,6]
+let res=val.myRed((num,acc)=>{
+    return acc + num
+},1)
+console.log("Reduce in Array",res);
+
+
+    ///Constructor in redecde
+
+class MyReduce {
+    constructor(value) {
+      this.value = value;
+    }
+    classOfReduce(callback, startingValue) {
+      let accumalator = startingValue;
+      for (let i = 0; i < this.value.length; i++) {
+        accumalator = callback(accumalator, this.value[i], i, this.value);
+      }
+      return accumalator;
+    }
+  }
+  
+  let arr4 = new MyReduce(arr);
+  let resultOfRed = arr4.classOfReduce((accu, num) => {
+   return accu + num;
+  }, 4);
+  console.log("Reduce in Construct", resultOfRed);
